60715a8acef885fa9e62f12d031f52616b029957
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2024-10-03 16:15:18 +0100

    MULTI-REPO_COMMIT: Move all extension reference guides to their designated page and rename README.adoc to index.adoc for friendlier URLS.
    Many small changes based on build output have also been done over many repositories. Some parts needed escaping, didn't have proper syntax, or headers that should really be tabs.

---
+++ localLinks/update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cloneOrPullMaster () {
+  if [ -d "$1" ]; then
+    echo "$1 does exist - pulling"
+      git pull origin $3
+      git checkout $3
+    else
+    echo "$1 does not exist - Setting up"
+      git clone --depth 30 --single-branch --branch $3 $2 $1
+  fi
+}
+
+cloneOrPullMaster "AxonFramework" "https://github.com/AxonFramework/AxonFramework.git" "axon-4.10.x"
+cloneOrPullMaster "extension-amqp" "https://github.com/AxonFramework/extension-amqp.git" "master"
+cloneOrPullMaster "extension-jgroups" "https://github.com/AxonFramework/extension-jgroups.git" "master"
+cloneOrPullMaster "extension-jobrunrpro" "https://github.com/AxonFramework/extension-jobrunrpro.git" "main"
+cloneOrPullMaster "extension-kafka" "https://github.com/AxonFramework/extension-kafka.git" "master"
+cloneOrPullMaster "extension-kotlin" "https://github.com/AxonFramework/extension-kotlin.git" "master"
+cloneOrPullMaster "extension-mongo" "https://github.com/AxonFramework/extension-mongo.git" "master"
+cloneOrPullMaster "extension-multitenancy" "https://github.com/AxonFramework/extension-multitenancy.git" "main"
+cloneOrPullMaster "extension-reactor" "https://github.com/AxonFramework/extension-reactor.git" "master"
+cloneOrPullMaster "extension-spring-aot" "https://github.com/AxonFramework/extension-spring-aot.git" "main"
+cloneOrPullMaster "extension-springcloud" "https://github.com/AxonFramework/extension-springcloud.git" "master"
+cloneOrPullMaster "extension-tracing" "https://github.com/AxonFramework/extension-tracing.git" "master"
+cloneOrPullMaster "axon-server" "https://github.com/AxonIQ/axon-server.git" "master"
+cloneOrPullMaster "axon-server-image-build" "https://github.com/AxonIQ/axon-server-image-build.git" "main"
+cloneOrPullMaster "axon-synapse" "https://github.com/AxonIQ/axon-synapse.git" "master"
+cloneOrPullMaster "console-web" "https://github.com/AxonIQ/console-web.git" "main"
+cloneOrPullMaster "giftcard-demo" "https://github.com/AxonIQ/giftcard-demo.git" "master"
+cloneOrPullMaster "bike-rental-quick-start" "https://github.com/AxonIQ/bike-rental-quick-start.git" "main"
+
+echo "You should be up-to-date now!"
